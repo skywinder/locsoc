1d8c19d6a503c3144fc0d5ee0182b8f4933ace16
Petr Korolev <sky4winder@gmail.com>
2019-12-13 13:58:51 +0700

    cm

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-
 require('dotenv').config();
-
-const result = dotenv.config()
  
-if (result.error) {
-  throw result.error
-}
- 
-console.log(result.parsed)
 const api_key = process.env.API_KEY;
 
 console.log(api_key);
 var date = Date();
-
-new Date().toLocaleTimeString(); // 11:18:48 AM
-//---
-new Date().toLocaleDateString(); // 11/16/2015
-//---
-new Date().toLocaleString(); // 11/16/2015, 11:18:48 PM
-
 
 console.log("\n" + date);
