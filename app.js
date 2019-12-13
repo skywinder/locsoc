9bf232d5e758a0fb0f0a073823aa03a86a881555
Petr Korolev <sky4winder@gmail.com>
2019-12-13 13:41:51 +0700

    cm

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 
 require('dotenv').config();
-console.log("fetch env");
+
+const result = dotenv.config()
+ 
+if (result.error) {
+  throw result.error
+}
+ 
+console.log(result.parsed)
 const api_key = process.env.API_KEY;
 
-console.log(api_key)
+console.log(api_key);
+var date = Date();
+
+new Date().toLocaleTimeString(); // 11:18:48 AM
+//---
+new Date().toLocaleDateString(); // 11/16/2015
+//---
+new Date().toLocaleString(); // 11/16/2015, 11:18:48 PM
+
+
+console.log("\n" + date);
